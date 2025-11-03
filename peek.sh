280097d15be4830ab48548ddcf1d014fd67d8e0b
leyre <leyre.roca326@gmail.com>
2025-11-03 11:24:44 +0100

    tarea 31-oct

--- peek.sh
+++ peek.sh
@@ -1,3 +1,16 @@
+nlines=$(cat $1 | wc -l)
+if [[ -z $2 ]]
+then 
+head -n3 $1
+echo "..."
+tail -n3 $1 
+elif [[ $nlines -gt $(( $2*2 )) ]]
+then
+echo Warning: this is not the full content of the file
 head -n $2 $1
 echo "..."
-tail -n $2 $1
+tail -n $2 $1 
+elif [[ $nlines -le $(( $2*2 )) ]]
+then
+cat $1
+fi
