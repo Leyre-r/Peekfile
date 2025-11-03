b080170bcce3c5fdf1a91c99dc75f0e616dd3ecd
leyre <leyre.roca326@gmail.com>
2025-11-03 12:39:51 +0100

    tarea 31-oct

--- countlines.sh
+++ countlines.sh
@@ -1,5 +1,7 @@
-echo $1
-nlines=$(cat $1 | wc -l )
+for i in $@
+do
+nlines=$(cat $i | wc -l )
+echo $i
 if [[ $nlines -eq 0 ]]
 then 
 echo the file has 0 lines
@@ -8,3 +10,4 @@ then echo the file has 1 line
 elif [[ $nlines -gt 1 ]]
 then echo the file has $nlines lines
 fi
+done
